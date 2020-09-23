a94f0cf94648e65bd7c37e6762df8229280deed0
Juan Eduardo Lopez Jaime <oDrack0@hotmail.com>
2020-09-23 13:39:57 -0500

    Creacion de capas del proyecto base

---
+++ CLLAIntelligent/CLLAIntelligentWEBApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace CLLAIntelligentWEBApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
